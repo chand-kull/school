fa13ff1028deecbeb7f746ae0b40bef981ba886d
chand-kull <cheerlachandrasekhar81@gmail.com>
2025-03-20 16:41:51 +0530

    some massage

---
+++ 03shall.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo " hey hai this is chandu2"
+
+
+echo " hey hai this is chandu2"#!/bin/bash
+
+echo " hey hai this is chandu2"#!/bin/bash
+
+echo " hey hai this is chandu2"v#!/bin/bash
+
+echo " hey hai this is chandu2"#!/bin/bash
+
+echo " hey hai this is chandu2"#!/bin/bash
+
+echo " hey hai this is chandu2"#!/bin/bash
+
+echo " hey hai this is chandu2"

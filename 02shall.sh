15957c3ea48be0f067bb4592c2d3a70684a50c95
chand-kull <cheerlachandrasekhar81@gmail.com>
2025-03-20 17:08:13 +0530

    some massage

--- 02shall.sh
+++ 02shall.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo " hey hai this is chandu2"
+echo "Ramesh :: hey hai suresh"
+echo "suresh :: hey hai ramesh how are you " 
+echo "ramesh :: i am good suresh what about you"
+echo "suresh :: i ma to good "
+echo "nice"
--- 04shall.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo " hey hai this is chandu2skfkjgjsgj11111111111111"

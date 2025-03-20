4d97e7bb611874e73492ac43ec4228729b04bb5d
chand-kull <cheerlachandrasekhar81@gmail.com>
2025-03-20 17:35:36 +0530

    some massage

---
+++ 03shall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=suresh
+
+
+echo "$PERSON1 :: hey hai $PERSON2"
+echo "$PERSON2 :: hey hai $PERSON1 how are you " 
+echo "$PERSON1 :: i am good $PERSON2 what about you"
+echo "$PERSON2 :: i ma to good "
+echo "nice"
--- 06shall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Ramesh :: hey hai suresh"
-echo "suresh :: hey hai ramesh how are you " 
-echo "ramesh :: i am good suresh what about you"
-echo "suresh :: i ma to good "
-echo "nice"

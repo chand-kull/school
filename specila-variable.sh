06610d97e67c5e81e4b6149f1128af313691be7f
chand-kull <cheerlachandrasekhar81@gmail.com>
2025-03-21 17:37:48 +0530

    some msg

---
+++ specila-variable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "all variables: $@"
+echo "number of variabes pass: $#"
+echo "script name: $0"
+echo " current working directory: $PWD"
+echo "home direcory of the current user : $HOME"
+echo "which user is runninf this script; $USER"
+echo "hostname; $HOSTNAME"
+echo "process id of the corrent shall script $$"
+sleep 60
+echo "process id of last background command ; $!"
+echo exit status kosam; $?""

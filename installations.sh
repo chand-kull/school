4f6725b086f953cc19f16009329794a02bd92fd5
chand-kull <cheerlachandrasekhar81@gmail.com>
2025-03-21 18:28:05 +0530

    some msg

---
+++ installations.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0]
+then    
+    echo " please run this script with root access."
+    exit 1
+echo " you are super user."
+fi
+
+dnf instll mysql -y
+ if [ $? -ne 0]
+ then 
+    echo "installation of mysql...FAILURE"
+    exit 1
+ fi
+

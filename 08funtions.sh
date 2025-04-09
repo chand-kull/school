db517b833499cf59573c39f915831f687cbfcbe5
chand-kull <cheerlachandrasekhar81@gmail.com>
2025-04-09 15:41:44 +0530

    hello script

---
+++ 08funtions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+VALIDATE(){
+    echo "Exit status:$1"
+    echo "what are you doing: $2"
+}
+
+if [ $USERID -ne 0 ]
+then 
+    echo "please run this script with root assess"
+    exit 1
+else
+    echo "you are super user"
+fi
+
+yum install mysql -y
+VALIDATE $? "Installing git"
+
+yum install mysql -y
+VALIDATE $? "Installing git"
